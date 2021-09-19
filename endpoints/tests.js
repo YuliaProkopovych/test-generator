91af81cc5ce234ed0162bc843b27e1f6c95aa683
Yulia <schonebud@gmail.com>
2021-09-19 16:35:23 +0300

    create get test by id route

--- endpoints/tests.js
+++ endpoints/tests.js
@@ -23,8 +23,44 @@ const getTestsOpts = {
 
 const testsRoute = (fastify, options, done) => {
   fastify.get('/tests', getTestsOpts);
+  fastify.get('/tests/:id', getTestOpts); // the :id route is a placeholder for an id (indicates a parameter)
 
   done();
 };
+
+const getTestOpts = {
+  schema: {
+    params: {
+      id: { type: 'number' },
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          id: { type: 'number' },
+          title: { type: 'string' },
+          body: { type: 'string' },
+        },
+      },
+    },
+  },
+  handler: (req, reply) => {
+    const { id } = req.params;
+
+    const test = tests.filter((test) => {
+      return test.id === id;
+    })[0];
+
+    if (!test) {
+      return reply.status(404).send({
+        errorMsg: 'Test not found',
+      });
+    }
+
+    return reply.send(test);
+  }
+
+};
+
 
 module.exports = testsRoute;
